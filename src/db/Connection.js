1a61683cf0d670273e595752ffffe0a4ce222bd6
saptaksengupta <deeps.sengupta@gmail.com>
2023-02-05 21:17:25 +0530

    fix: minor fixes

--- src/db/Connection.js
+++ src/db/Connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const uri = "mongodb+srv://saptaksengupta:kUr4lIBh8qeiDrVl@blogapp.2yuj3js.mongodb.net/BlogApp?retryWrites=true&w=majority";
+const uri = "mongodb+srv://saptaksengupta:<password>@blogapp.2yuj3js.mongodb.net/BlogApp?retryWrites=true&w=majority";
 // Establish a connection with the database.
 const connectDb = () => {
     return mongoose.connect(uri, {
